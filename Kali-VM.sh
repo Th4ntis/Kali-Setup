fac6c54ee0715092efd727ea3715850b7f1f44f5
Th4ntis <53808039+Th4ntis@users.noreply.github.com>
2024-06-11 20:09:34 +0000

    Update Kali-VM.sh

--- Kali-VM.sh
+++ Kali-VM.sh
@@ -49,7 +49,7 @@ cp -r ~/Kali-Setup/tmux/tmux-cpu ~/.tmux/plugins/
 cp -r ~/Kali-Setup/tmux/tmux-yank ~/.tmux/plugins/
 echo -e "Copying fonts..."
 sudo mkdir /usr/share/fonts/truetype/MesloLGS
-sudo cp ~/Kali-Setup/Fonts/* /usr/share/fonts/truetype/MesloLGS/
+sudo cp ~/Kali-Setup/fonts/* /usr/share/fonts/truetype/MesloLGS/
 echo -e "Copying fusuma config..."
 mkdir ~/.config/fusuma
 cp -r ~/Kali-Setup/fusuma/config.yml ~/.config/fusuma/
