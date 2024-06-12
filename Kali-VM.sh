e44ee7cb9b26e1f87bd555d45f08a26c91262bf0
Th4ntis <53808039+Th4ntis@users.noreply.github.com>
2024-06-12 21:42:09 +0000

    Update Kali-VM.sh

--- Kali-VM.sh
+++ Kali-VM.sh
@@ -10,21 +10,20 @@ green='\e[1;33m[+]\e[0m'
 
 clear
 
-echo -e "\n$green Running apt update..."
-sudo apt-get update
-echo -e "$green Complete"
-
-echo -e "\n$green Running apt upgrade..."
+echo -e "\n$green Running sudo apt update and sudo apt-upgrade..."
+sudo apt-get -qq update
 sudo apt-get -y -qq upgrade
 echo -e "$green Complete"
 
 echo -e "\n$green Installing tools via apt-get..."
-sleep 3
-sudo apt install -y -qq terminator neo4j bloodhound bloodhound.py amass pipx netexec libu2f-udev xclip realtek-rtl88xxau-dkms dkms libcurl4-openssl-dev libssl-dev zlib1g-dev libnetfilter-queue-dev libusb-1.0-0-dev libpcap-dev flameshot bridge-utils xfce4-dev-tools pkg-config golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev docker.io docker-compose golang-go gpsd gpsd-clients gpsd-tools autorecon
+sudo apt install -y -qq terminator neo4j bloodhound bloodhound.py amass pipx netexec libu2f-udev xclip realtek-rtl88xxau-dkms dkms libcurl4-openssl-dev libssl-dev zlib1g-dev libnetfilter-queue-dev libusb-1.0-0-dev libpcap-dev flameshot bridge-utils xfce4-dev-tools pkg-config golang-gir-gio-2.0-dev libgtk-3-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev docker.io docker-compose golang-go gpsd gpsd-clients gpsd-tools xorg xrdp autorecon
+echo -e "$green Complete"
+
+echo -e "\n$green Setting up RDP over port 3389"
+sudo sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
 echo -e "$green Complete"
 
 echo -e "\n$green Making tools folder under ~/Tools"
-sleep 3
 mkdir ~/Tools
 
 echo -e "\n$green Copying .zshrc to .zshrc.bak"
@@ -33,16 +32,19 @@ cp ~/.zshrc .zshrc.bak
 echo -e "\n$green Getting config/dot files..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k > /dev/null
+#git clone --quiet https://github.com/Th4ntis/Kali-Setup.git ~/Kali-Setup > /dev/null
 
-git clone --quiet https://github.com/Th4ntis/Kali-Setup.git ~/Kali-Setup > /dev/null
-echo -e "Copying zshrc..."
+echo -e "\n$green Copying zshrc..."
 cp ~/Kali-Setup/zsh/zshrc ~/.zshrc
-echo -e "Copying aliases..."
+
+echo -e "\n$green Copying aliases..."
 cp ~/Kali-Setup/zsh/aliases ~/.aliases
-echo -e "Copying terminator config..."
+
+echo -e "\n$green Copying terminator config..."
 mkdir ~/.config/terminator
 cp ~/Kali-Setup/terminator/config ~/.config/terminator/config
-echo -e "Copying tmux files and plugins..."
+
+echo -e "\n$green Copying tmux files and plugins..."
 cp ~/Kali-Setup/tmux/tmux.conf ~/.tmux.conf
 mkdir ~/.tmux
 mkdir ~/.tmux/plugins
@@ -50,28 +52,25 @@ cp -r ~/Kali-Setup/tmux/tpm ~/.tmux/plugins/
 cp -r ~/Kali-Setup/tmux/tmux-battery ~/.tmux/plugins/
 cp -r ~/Kali-Setup/tmux/tmux-cpu ~/.tmux/plugins/
 cp -r ~/Kali-Setup/tmux/tmux-yank ~/.tmux/plugins/
-echo -e "Copying fonts..."
+
+echo -e "\n$green Copying fonts..."
 sudo mkdir /usr/share/fonts/truetype/MesloLGS
 sudo cp ~/Kali-Setup/fonts/* /usr/share/fonts/truetype/MesloLGS/
 
-echo -e "Setting Wallapaper..."
+echo -e "\n$green Setting Wallapaper..."
 sudo wget -q -O /usr/share/backgrounds/th4ntis.png https://raw.githubusercontent.com/th4ntis/Kali-Setup/main/images/CyberSpider-UG-Outline.png
 for i in $(xfconf-query -c xfce4-desktop -lv | grep last-image | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $i -s /usr/share/backgrounds/th4ntis.png; done
 for i in $(xfconf-query -c xfce4-desktop -lv | grep image-style | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $i -s 4; done
 for i in $(xfconf-query -c xfce4-desktop -lv | grep color-style | awk '{print $1}'); do xfconf-query -c xfce4-desktop -p $i -s 0; done
 for i in $(xfconf-query -c xfce4-desktop -lv | grep rgba1 | awk '{print $1}'); do  xfconf-query -c xfce4-desktop -p $i -t double -s 0 -t double -s 0 -t double -s 0 -t double -s 1; done
 
-echo -e "Setting LightDM Greeting settings..."
+ehco -e "\n$green Setting LightDM Greeting settings..."
 sudo wget -q -O /usr/share/backgrounds/kali-linux.png https://raw.githubusercontent.com/th4ntis/Kali-Setup/main/images/Kali-Linux.png
 sudo sed -i 's|/usr/share/desktop-base/kali-theme/login/background|/usr/share/backgrounds/kali-linux.png|g' /etc/lightdm/lightdm-gtk-greeter.conf
 sudo sed -i 's|theme-name = Kali-Light|theme-name = Kali-Dark|g' /etc/lightdm/lightdm-gtk-greeter.conf
 sudo sed -i 's|font-name = Cantarell 11|font-name = Hack 11|g' /etc/lightdm/lightdm-gtk-greeter.conf
 sudo sed -i 's|icon-theme-name = Flat-Remix-Blue-Light|icon-theme-name = Flat-Remix-Blue-Dark|g' /etc/lightdm/lightdm-gtk-greeter.conf
 sudo sed -i 's|default-user-image = #emblem-kali|default-user-image = /usr/share/backgrounds/th4ntis.png|g' /etc/lightdm/lightdm-gtk-greeter.conf
-
-sudo sed -i 's|GRUB_TIMEOUT=5|GRUB_TIMEOUT=0|g' /etc/lightdm/lightdm-gtk-greeter.conf
-
-sudo update-grub
 
 echo -e "\n$green adding GOPATH to .zshrc"
 echo '# GO Path' >> ~/.zshrc
@@ -84,19 +83,9 @@ echo -e "\n$green Adding pipx path to .zshrc"
 echo '# PIPX Path' >> ~/.zshrc
 echo 'export PATH="$PATH:/home/th4ntis/.local/bin"' >> ~/.zshrc
 echo 'export PATH="$PATH:/root/.local/bin"' >> ~/.zshrc
-sudo pipx ensurepath
 echo -e "$green Complete"
 
 source ~/.zshrc
-
-echo -e "\n$green Installing Google Chrome..."
-wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Chrome.deb
-sudo dpkg -i ~/Chrome.deb;sudo apt install -y -f 2> /dev/null
-sudo rm -r ~/Chrome.deb
-echo -e "$green Complete"
-
-echo -e "\n$green Unzipping rockyou.txt"
-cd /usr/share/wordlists && sudo gzip -dqf /usr/share/wordlists/rockyou.txt.gz
 
 echo -e "\n$green Getting Responder as backup to installed Responder in directory ~/Tools/"
 git clone --quiet https://github.com/lgandx/Responder.git ~/Tools/Responder > /dev/null
@@ -122,6 +111,24 @@ echo -e "\n$green Getting gnmap parser in directory ~/Tools/"
 git clone --quiet https://github.com/jasonjfrank/gnmap-parser.git ~/Tools/gnmap-parser > /dev/null
 echo -e "$green Complete"
 
+echo -e "\n$green Installing Google Chrome..."
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Chrome.deb
+sudo dpkg -i ~/Chrome.deb;sudo apt install -y -f 2> /dev/null
+sudo rm -r ~/Chrome.deb
+echo -e "$green Complete"
+
+echo -e "\n$green Installing ShodanCLI"
+pipx install shodan
+sudo pipx install shodan
+echo -e "$green Complete"
+
+echo -e "\n$green Installing DonPAPI"
+pipx install donpapi
+sudo pipx install donpapi
+echo -e "$green Complete"
+
+sudo pipx ensurepath
+
 echo -e "\n$green Installing Bettercap and Bettercap WebUI..."
 go install github.com/bettercap/bettercap@latest
 sudo ~/.go/bin/bettercap -eval "caplets.update; ui.update; q"
@@ -140,16 +147,6 @@ echo -e "$green Complete"
 echo -e "\n$green Installing HCXTools"
 git clone --quiet https://github.com/ZerBea/hcxtools.git ~/Tools/hcxtools > /dev/null
 cd ~/Tools/hcxtools&& sudo make && sudo make install
-echo -e "$green Complete"
-
-echo -e "\n$green Installing ShodanCLI"
-pipx install shodan
-sudo pipx install shodan
-echo -e "$green Complete"
-
-echo -e "\n$green Installing DonPAPI"
-pipx install donpapi
-sudo pipx install donpapi
 echo -e "$green Complete"
 
 echo -e "\n$green Installing Nessus"
@@ -172,10 +169,7 @@ echo -e "$green Complete"
 #wget -q https://raw.githubusercontent.com/th4ntis/Kali-Setup/xfce/xfce4-panel.xml -O /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
 #echo -e "$green Complete"
 
-echo -e "\n$green Installing XRDP and ZORG for RDP"
-sudo apt install -y -qq xrdp xorg
-
-echo -e "\n$green Changing RDP Port to 3390"
-sudo sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
+#echo -e "\n$green Unzipping rockyou.txt"
+#cd /usr/share/wordlists && sudo gzip -dqf /usr/share/wordlists/rockyou.txt.gz
 
 echo -e "\n$green ===== All finished! Reboot for any/all changes to take affect ===== $green\n"
