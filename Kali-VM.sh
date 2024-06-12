dc841279f82219903d1bae8dcc553f823af6f044
Th4ntis <53808039+Th4ntis@users.noreply.github.com>
2024-06-12 19:32:10 +0000

    Update Kali-VM.sh

--- Kali-VM.sh
+++ Kali-VM.sh
@@ -160,17 +160,17 @@ sudo dpkg -i /tmp/nessus_amd64.deb
 sudo rm -f /tmp/nessus_amd64.deb
 echo -e "$green Complete"
 
-echo -e "\n$green Changing Power Settings"
-wget -q https://raw.githubusercontent.com/th4ntis/Kali-Setup/xfce/xfce4-power-manager.xml -O /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
-echo -e "$green Complete"
+#echo -e "\n$green Changing Power Settings"
+#wget -q https://raw.githubusercontent.com/th4ntis/Kali-Setup/xfce/xfce4-power-manager.xml -O /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml
+#echo -e "$green Complete"
 
-echo -e "\n$green Changing XFCE Shotcuts"
-wget -q https://raw.githubusercontent.com/th4ntis/Kali-Setup/xfce/xfce-shortcuts.xml -O /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce-shortcuts.xml
-echo -e "$green Complete"
+#echo -e "\n$green Changing XFCE Shotcuts"
+#wget -q https://raw.githubusercontent.com/th4ntis/Kali-Setup/xfce/xfce-shortcuts.xml -O /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce-shortcuts.xml
+#echo -e "$green Complete"
 
-echo -e "\n$green Changing XFCE Panel"
-wget -q https://raw.githubusercontent.com/th4ntis/Kali-Setup/xfce/xfce4-panel.xml -O /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
-echo -e "$green Complete"
+#echo -e "\n$green Changing XFCE Panel"
+#wget -q https://raw.githubusercontent.com/th4ntis/Kali-Setup/xfce/xfce4-panel.xml -O /home/$USER/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+#echo -e "$green Complete"
 
 echo -e "\n$green Installing XRDP and ZORG for RDP"
 sudo apt install -y -qq xrdp xorg
